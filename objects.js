08992972b85265c75037410cb12d2cd6264ba185
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-05-01 08:55:16 +0300

    first commit

---
+++ objects.js
@@ -0,0 +1,74 @@
+//1. Create a function called getAvailableBooks that returns an array of all available
+//books.
+const books = [
+{ title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+isAvailable: true },
+{ title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+isAvailable: false },
+{ title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+1866, isAvailable: true },
+{ title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+isAvailable: false },
+{ title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+];
+function getAvailableBooks(books){
+    books.filter(function(book){
+        return getAvailableBooks.title
+    }
+    )}.map(function(book){
+        return getAvailableBooks.book
+    })
+    console.log(getBooksByAuthor(authors));
+
+    
+   
+
+console.log(getAvailableBooks(books));
+
+//Create a function getBooksByAuthor that takes an author's name as an argument and
+//returns an array of all books by that author.
+function getBooksByAuthor(authors){
+    books.filter(function(book){
+        return books.book
+    }
+)}
+console.log(getBooksByAuthor(authors));
+//Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+//publicationYear, and isAvailable).
+function addNewBook(books){
+    const books={}
+    books.addNewBook=function(){console.log(addNewBook(books));}
+    addNewBook.title="Born A crime"
+    addNewBook.author="Mary Ann"
+    addNewBook.publicationYear=1980
+    addNewBook.isAvailable=year
+    
+
+    
+
+}
+console.log(addNewBook(books));
+
+//Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+function checkoutBook(title){
+    books.filter(function(book){
+        return books.title
+
+    }
+
+)}
+
+//Create a function returnBook that takes a book title as an argument and changes the
+//book's isAvailable property to true. If the book is not found in the library, the function
+//should return a message indicating that the book does not belong to the library.
+function returnBook(title){
+    returnBook.filter(function(book){
+    return True
+
+    })
+
+}
+console.log(returnBook(title));
